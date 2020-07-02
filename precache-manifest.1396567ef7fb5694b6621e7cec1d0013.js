9c0f52fe74e824b14d4f1c69b14c59fa8843be13
jpaaaps <jpgerard87@gmail.com>
2020-07-02 16:58:17 +0200

    Updates

---
+++ precache-manifest.1396567ef7fb5694b6621e7cec1d0013.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3047184f33e91cf89bc9e109c307c9c",
+    "url": "/Jpaaaps/my_portfolio/index.html"
+  },
+  {
+    "revision": "4f29eddd89e49e6904e9",
+    "url": "/Jpaaaps/my_portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "57df8d291a90b5eec97c",
+    "url": "/Jpaaaps/my_portfolio/static/js/2.01b6c9ae.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Jpaaaps/my_portfolio/static/js/2.01b6c9ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f29eddd89e49e6904e9",
+    "url": "/Jpaaaps/my_portfolio/static/js/main.d49a174c.chunk.js"
+  },
+  {
+    "revision": "8a815abca6a88b1c4ae1",
+    "url": "/Jpaaaps/my_portfolio/static/js/runtime-main.843325fc.js"
+  }
+]);
